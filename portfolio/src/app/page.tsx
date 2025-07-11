e0e52f6d0b066a7ff6fc17067f6c8c83f4ac147d
Lance Takiguchi <lancetakiguchi@gmail.com>
2025-07-11 15:35:56 -0700

    Add footer linkedin logo

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+// import Image from 'next/image'; // If you're using Next.js Image component for optimization
+import LinkedInSvg from './assets/LinkedIn.svg';
 
 export default function Home() {
   return (
@@ -114,48 +115,11 @@ export default function Home() {
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://www.linkedin.com/in/lancetakiguchi"
           target="_blank"
-          rel="noopener noreferrer"
+          rel=""
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          <LinkedInSvg width={16} height={16} aria-hidden />
         </a>
       </footer>
     </div>
