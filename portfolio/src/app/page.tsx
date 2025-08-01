9510932afb0235afff7569f22b65c80ab300b751
Lance Takiguchi <lancetakiguchi@gmail.com>
2025-08-01 15:23:44 -0700

    mobile fixes

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -5,7 +5,7 @@ import WPSDKPng from './assets/page/SquareWPSDKReference.png';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-space-grotesk)]">
+    <div className="grid sm:grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 sm:gap-16 sm:p-20 font-[family-name:var(--font-space-grotesk)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="
         bg-gradient-to-r from-midnightNebula to-midnightNebula-900
@@ -209,7 +209,7 @@ export default function Home() {
           </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="p-8 row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://www.linkedin.com/in/lancetakiguchi"
